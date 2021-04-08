0eb33a79d83ced19df7f987c53118fc2cacab179
Fellipe Saraiva <fellipe.saraiva@netscanner.com.br>
2021-04-08 20:49:44 -0300

    Adicionando commit inicial com index, bootstrap e NPM

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS do Bootstrap -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap-4.6.0-dist/css/bootstrap.min.css">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="bootstrap-4.6.0-dist/js/bootstrap.min.js"></script>
+    
+    <title>Login!</title>
+  </head>
+  <body>
+    <div class='container-fluid'>
+        <div class='row'>
+            <div class='col-md-4'></div>
+            <div class='col-md-4' style='background-color: grey;'>
+                <label>Digite seu login:</label>
+                <input type='text' name='login' class='form-control'>
+                <label>Digite sua senha:</label>
+                <input type='password' name='login' class='form-control'>
+            </div>
+            <div class='col-md-4'></div>
+        </div>
+    </div>
+  </body>
+</html>
